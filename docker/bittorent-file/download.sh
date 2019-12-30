0cde4f1f5bb8063e7f684051d8b35a65eff2817f
Alexander Korolev <kilork@yandex.ru>
2019-12-30 14:50:39 +0300

    switch docker tracker to opentracker

--- docker/bittorent-file/download.sh
+++ docker/bittorent-file/download.sh
@@ -3,7 +3,7 @@
 while true; do
     if [[ ! -f linux-5.1.16.tar.xz ]]; then
         curl -v https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.16.tar.xz -o linux-5.1.16.tar.xz
-        mktorrent -a http://bttracker:8000/announce linux-5.1.16.tar.xz
+        mktorrent -a http://bttracker:6969/announce linux-5.1.16.tar.xz
     fi
 
     if [[ ! -f /data/qbittorrent/linux-5.1.16.tar.xz ]]; then
@@ -12,7 +12,7 @@ while true; do
     fi
 
     if [[ ! -f linux-5.1.16.tar.xz.torrent ]]; then
-        mktorrent -a http://bttracker:8000/announce linux-5.1.16.tar.xz
+        mktorrent -a http://bttracker:6969/announce linux-5.1.16.tar.xz
     fi
 
     if [[ ! -f /data/watch/linux-5.1.16.tar.xz.torrent ]]; then
