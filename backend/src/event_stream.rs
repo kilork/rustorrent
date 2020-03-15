376f7edbcf5f22dfdae9a01ea3fc000fdd752e9b
Alexander Korolev <kilork@yandex.ru>
2020-03-15 22:41:53 +0100

    merge cmd line option to settings

---
+++ backend/src/event_stream.rs
@@ -0,0 +1,69 @@
+use super::*;
+
+#[get("/stream")]
+pub(crate) async fn stream(broadcaster: web::Data<RwLock<Broadcaster>>) -> impl Responder {
+    let rx = broadcaster.write().await.new_client();
+    HttpResponse::Ok()
+        .content_type("text/event-stream")
+        .keep_alive()
+        .no_chunking()
+        .streaming(rx)
+}
+
+pub(crate) struct Broadcaster {
+    pub(crate) clients: Vec<Sender<Bytes>>,
+}
+
+impl Broadcaster {
+    pub(crate) fn new() -> Self {
+        Self { clients: vec![] }
+    }
+
+    pub(crate) fn new_client(&mut self) -> Client {
+        eprintln!("adding new client");
+        let (tx, rx) = mpsc::channel(100);
+
+        tx.clone()
+            .try_send(Bytes::from("data: connected\n\n"))
+            .unwrap();
+
+        self.clients.push(tx);
+
+        Client(rx)
+    }
+
+    pub(crate) fn message(&mut self, msg: &str) -> Result<(), Vec<Sender<Bytes>>> {
+        let mut ok_clients = vec![];
+
+        debug!("message to {} client(s)", self.clients.len());
+
+        let msg = Bytes::from(["data: ", msg, "\n\n"].concat());
+
+        for client in &mut self.clients {
+            if let Ok(()) = client.try_send(msg.clone()) {
+                ok_clients.push(client.clone())
+            }
+        }
+
+        if ok_clients.len() != self.clients.len() {
+            return Err(ok_clients);
+        }
+
+        Ok(())
+    }
+}
+
+// wrap Receiver in own type, with correct error type
+pub(crate) struct Client(Receiver<Bytes>);
+
+impl Stream for Client {
+    type Item = Result<Bytes, Error>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        match Pin::new(&mut self.0).poll_next(cx) {
+            Poll::Ready(Some(v)) => Poll::Ready(Some(Ok(v))),
+            Poll::Ready(None) => Poll::Ready(None),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -42,10 +42,12 @@ use tokio::{
 use url::Url;
 
 mod cli;
+mod event_stream;
 mod login;
 mod model;
 mod uploads;
 
+use event_stream::*;
 use login::*;
 use model::*;
 use uploads::*;
@@ -203,16 +205,6 @@ fn host(path: &str) -> String {
     RSBT_UI_HOST.clone() + path
 }
 
-#[get("/stream")]
-async fn stream(broadcaster: web::Data<RwLock<Broadcaster>>) -> impl Responder {
-    let rx = broadcaster.write().await.new_client();
-    HttpResponse::Ok()
-        .content_type("text/event-stream")
-        .keep_alive()
-        .no_chunking()
-        .streaming(rx)
-}
-
 #[actix_rt::main]
 async fn main() -> Result<(), ExitFailure> {
     let cli = cli::Cli::from_args();
@@ -233,7 +225,10 @@ async fn main() -> Result<(), ExitFailure> {
 
     let client = web::Data::new(client);
 
-    let settings = Settings::default();
+    let settings = Settings::default().override_with(cli.config);
+
+    debug!("starting torrents process with settings: {:?}", settings);
+
     let rsbt_app = web::Data::new(RsbtApp::new(settings));
     let broadcaster = web::Data::new(RwLock::new(Broadcaster::new()));
 
@@ -324,62 +319,4 @@ async fn main() -> Result<(), ExitFailure> {
     .await?;
 
     Ok(())
-}
-
-struct Broadcaster {
-    clients: Vec<Sender<Bytes>>,
-}
-
-impl Broadcaster {
-    fn new() -> Self {
-        Self { clients: vec![] }
-    }
-
-    fn new_client(&mut self) -> Client {
-        eprintln!("adding new client");
-        let (tx, rx) = mpsc::channel(100);
-
-        tx.clone()
-            .try_send(Bytes::from("data: connected\n\n"))
-            .unwrap();
-
-        self.clients.push(tx);
-
-        Client(rx)
-    }
-
-    fn message(&mut self, msg: &str) -> Result<(), Vec<Sender<Bytes>>> {
-        let mut ok_clients = vec![];
-
-        debug!("message to {} client(s)", self.clients.len());
-
-        let msg = Bytes::from(["data: ", msg, "\n\n"].concat());
-
-        for client in &mut self.clients {
-            if let Ok(()) = client.try_send(msg.clone()) {
-                ok_clients.push(client.clone())
-            }
-        }
-
-        if ok_clients.len() != self.clients.len() {
-            return Err(ok_clients);
-        }
-
-        Ok(())
-    }
-}
-
-// wrap Receiver in own type, with correct error type
-struct Client(Receiver<Bytes>);
-
-impl Stream for Client {
-    type Item = Result<Bytes, Error>;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        match Pin::new(&mut self.0).poll_next(cx) {
-            Poll::Ready(Some(v)) => Poll::Ready(Some(Ok(v))),
-            Poll::Ready(None) => Poll::Ready(None),
-            Poll::Pending => Poll::Pending,
-        }
-    }
 }
