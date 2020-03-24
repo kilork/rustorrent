6ea0a6461565971821654cf7ea161489571e3321
S4ltyGo4t <mario.weidner@gmx.de>
2020-03-24 21:16:29 +0100

    removed unused imports

--- frontend/www/src/main/webapp/app/entities/torrent/torrent-update.component.ts
+++ frontend/www/src/main/webapp/app/entities/torrent/torrent-update.component.ts
@@ -4,8 +4,6 @@ import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
-import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
 
 import { ITorrent, Torrent } from 'app/shared/model/torrent.model';
 import { TorrentService } from './torrent.service';
