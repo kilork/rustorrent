84e6bb3d8b6b3c3a8460a65fbd61b304f5e0b799
Alexander Korolev <kilork@yandex.ru>
2020-04-06 20:45:22 +0200

    send peers with delay

--- service/src/app/download_torrent/process_announce.rs
+++ service/src/app/download_torrent/process_announce.rs
@@ -6,10 +6,12 @@ pub(crate) async fn process_announce(
 ) -> Result<(), RsbtError> {
     let mut download_torrent_broker_sender = torrent_process.broker_sender.clone();
 
-    for peer in peers.into_iter().take(1) {
+    for peer in peers {
         download_torrent_broker_sender
             .send(DownloadTorrentEvent::PeerAnnounced(peer))
             .await?;
+
+        delay_for(Duration::from_secs(1)).await;
     }
 
     Ok(())
