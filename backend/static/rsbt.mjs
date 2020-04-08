5f0ac07549726c4e0cd27ca1bc8f886f19411593
Alexander Korolev <kilork@yandex.ru>
2020-04-09 01:09:27 +0200

    sandbox: enable / disable

--- backend/static/rsbt.mjs
+++ backend/static/rsbt.mjs
@@ -14,17 +14,57 @@ function timeout(interval) {
 class TorrentService {
     constructor(torrentsTable) {
         this.torrentsTable = torrentsTable;
+        this.torrents = [];
     }
+
+    async enable(input) {
+        await this.action(input, "enable");
+    }
+
+    async disable(input) {
+        await this.action(input, "disable");
+    }
+
+    async action(input, action) {
+        let id = input.dataset.id;
+
+        let response = await fetch(`/api/torrent/${id}/action`, {
+            method: "POST",
+            body: JSON.stringify({
+                "action": action
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const torrent = await (await fetch(`/api/torrent/${id}`)).json();
+
+        if (torrent.error) {
+            throw torrent.error;
+        }
+
+        let updatedTorrent = this.torrents.find(t => `${t.data.id}` === id);
+
+        if (updatedTorrent) {
+            updatedTorrent.torrent = torrent;
+            updatedTorrent.element.innerHTML = this.torrentRow(torrent);
+        }
+    }
+
 
     torrentRow(torrent) {
         return [
             `${torrent.id}`,
             `<strong>${torrent.name}</strong><br><span class="size" title="Size">${torrent.length}</span>`,
             [
-                `<span class="upload"><span class="tx" title="Uploaded">⬆︎</span> ${torrent.tx}</span>`,
-                `<span class="download"><span class="rx" title="Downloaded">⬇︎</span> ${torrent.rx}</span>`,
-                `${torrent.pieces_left} / ${torrent.pieces_total}`
-            ].join('<br>')
+                `<div class="upload">${torrent.tx}</div>`,
+                `<div class="download">${torrent.rx}</div>`,
+                `<div class="ratio">${torrent.pieces_left}<span class="ratio-split"></span>${torrent.pieces_total}</div>`
+            ].join(''),
+            [
+                torrent.active ? `<input type="button" class="button-primary torrent-action torrent-action-disable" data-id="${torrent.id}" title="Pause" value="❚❚" onclick="torrentService.disable(this)">` : `<input type="button" class="button-primary torrent-action torrent-action-enable" data-id="${torrent.id}" title="Start" value="▶︎" onclick="torrentService.enable(this)">`
+            ].join('')
         ].map(value => `<td>${value}</td>`).join('')
     }
 
@@ -36,11 +76,16 @@ class TorrentService {
         }
 
         this.torrentsTable.innerHTML = '';
+        this.torrents = [];
 
         for (const torrent of torrents) {
             const newElement = document.createElement("tr");
             newElement.innerHTML = this.torrentRow(torrent);
             this.torrentsTable.appendChild(newElement);
+            this.torrents.push({
+                element: newElement,
+                data: torrent
+            })
         }
 
     }
